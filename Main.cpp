50b6d05ba4ab3153d0ab49432600cac6fbe3271c
ITSTEP\st <st>
2024-06-11 18:40:27 +0300

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,26 @@
+﻿#include "Tests.h"
+
+int main() {
+	testingExamTask();
+	cout << "test01 - " << test01() << endl;
+	cout << "test02 - " << test02() << endl;
+	cout << "test03 - " << test03() << endl;
+	cout << "test04 - " << test04() << endl;
+	cout << "test05 - " << test05() << endl;
+	cout << "test06 - " << test06() << endl;
+	cout << "test07 - " << test07() << endl;
+	cout << "test08 - " << test08() << endl;
+	cout << "test09 - " << test09() << endl;
+	cout << "test10 - " << test10() << endl;
+	cout << "test11 - " << test11() << endl;
+	cout << "test12 - " << test12() << endl;
+	cout << "test13 - " << test13() << endl;
+	cout << "test14 - " << test14() << endl;
+	cout << "test15 - " << test15() << endl;
+	cout << "test16 - " << test16() << endl;
+	cout << "test17 - " << test17() << endl;
+	cout << "test18 - " << test18() << endl;
+	cout << "test19 - " << test19() << endl;
+	cout << "test20 - " << test20() << endl;
+	return 0;
+}
